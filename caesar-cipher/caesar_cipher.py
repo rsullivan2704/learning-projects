046d05c2726836a8703aedd81e306861e11e6cbf
rsullivan2704 <rsullivan2704@gmail.com>
2018-12-27 21:42:09 -0500

    Added comments

--- caesar-cipher/caesar_cipher.py
+++ caesar-cipher/caesar_cipher.py
@@ -5,7 +5,9 @@ RIGHT = False
 class CaesarCipher(object):
     def __init__(self, plain_text:str, shift:int, direction:bool):
         self._plain_text = plain_text
+        # Normalize the shift to 0 <= shift < len(ALPHA)
         self._shift = shift % len(ALPHA)
+        # Set the direction and negate it if direction == LEFT
         self._direction = direction
         if direction == LEFT:
             self._shift *= -1        
@@ -13,6 +15,7 @@ class CaesarCipher(object):
     def decode(self, encoded_text:str):
         result = ''
         for char in encoded_text:
+            # Get the index by reversing the shift and normalizing to 0 <= index < len(ALPHA)
             index = (ALPHA.find(char) - self._shift) % len(ALPHA)
             result += ALPHA[index]
         return result
@@ -20,6 +23,7 @@ class CaesarCipher(object):
     def encode(self):
         result = ''
         for char in self._plain_text:
+            # Get the index by adding the shift and normalizing to 0 <= index < len(ALPHA)
             index = (ALPHA.find(char) + self._shift) % len(ALPHA)
             result += ALPHA[index]
         return result
